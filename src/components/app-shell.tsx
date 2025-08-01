7b8cd92e7cc1590b5c9a24618c43771c0571b6c4
Loki James <lordlooki18@gmail.com>
2025-08-01 14:59:35 +0000

    cambiar el logo por el siguiente <?xml version="1.0" standalone="no"?> <

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -222,7 +222,7 @@ export function AppShell({ children, user }: { children: React.ReactNode, user: 
         <div className="flex min-h-screen w-full">
             <Sidebar variant="sidebar" collapsible="icon" className="bg-primary text-primary-foreground">
                 <SidebarHeader>
-                    <div className="flex items-center justify-center h-16 w-32 group-data-[collapsible=icon]:w-10 transition-[width] duration-300 ease-in-out text-primary-foreground">
+                    <div className="flex items-center justify-center h-16 text-primary w-32 group-data-[collapsible=icon]:w-10 transition-[width] duration-300 ease-in-out">
                        <Logo className="w-full h-auto" />
                     </div>
                 </SidebarHeader>
