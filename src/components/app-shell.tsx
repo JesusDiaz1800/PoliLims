ab22cb3e0cb72d760b57e5b961e29811ad1fd1f8
Loki James <lordlooki18@gmail.com>
2025-08-06 20:34:01 +0000

    COMO HAGO PARA PLEGAR LA BARRA?

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -340,7 +340,7 @@ export function AppShell({ children, user }: { children: React.ReactNode, user: 
             <SidebarInset>
                 <header className="sticky top-0 z-30 flex h-16 items-center justify-between border-b bg-background/80 backdrop-blur-sm px-4 sm:px-6">
                     <div className="flex items-center gap-2">
-                        <SidebarTrigger className="md:hidden"/>
+                        <SidebarTrigger/>
                          <h1 className="text-xl font-semibold font-headline text-foreground">
                            {getPageTitle()}
                         </h1>
