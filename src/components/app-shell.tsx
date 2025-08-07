8dd5f6d275a102cafc0d50e67f92a79cff8ba3e4
Loki James <lordlooki18@gmail.com>
2025-08-07 20:04:55 +0000

    alinear correctamente el logo

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -263,8 +263,8 @@ export function AppShell({ children, user }: { children: React.ReactNode, user: 
     return (
         <div className="flex min-h-screen w-full bg-background">
             <Sidebar>
-                <SidebarHeader className="h-16 flex items-center px-2">
-                    <div className="flex items-center w-48 group-data-[state=collapsed]/sidebar-wrapper:w-auto group-data-[state=collapsed]/sidebar-wrapper:justify-center group-data-[state=collapsed]/sidebar-wrapper:px-0">
+                <SidebarHeader className="h-16 flex items-center p-2">
+                    <div className="w-48 group-data-[state=collapsed]/sidebar-wrapper:w-auto group-data-[state=collapsed]/sidebar-wrapper:justify-center group-data-[state=collapsed]/sidebar-wrapper:px-0">
                        <Logo />
                     </div>
                 </SidebarHeader>
