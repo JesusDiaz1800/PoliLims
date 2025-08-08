26029656611a92cbe0448e4c14bc3214f5bca944
Loki James <lordlooki18@gmail.com>
2025-08-08 13:46:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/reports/biblioteca-informes-table.tsx
+++ src/components/reports/biblioteca-informes-table.tsx
@@ -23,6 +23,8 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import {
