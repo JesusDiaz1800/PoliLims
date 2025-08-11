cc384c025a6c2e6af92b3a8ebac95d1ce35cbc2d
Loki James <lordlooki18@gmail.com>
2025-08-11 17:55:56 +0000

    nada de nada el menu sigue exactamente igual, yo digo que revises todo e

--- src/app/(app)/gestion/page.tsx
+++ src/app/(app)/gestion/page.tsx
@@ -6,5 +6,3 @@ import { redirect } from 'next/navigation';
 export default function GestionPage() {
   redirect('/equipos');
 }
-
-    
--- src/app/(app)/operaciones/page.tsx
+++ src/app/(app)/operaciones/page.tsx
@@ -6,5 +6,3 @@ import { redirect } from 'next/navigation';
 export default function OperacionesPage() {
   redirect('/importaciones');
 }
-
-    
