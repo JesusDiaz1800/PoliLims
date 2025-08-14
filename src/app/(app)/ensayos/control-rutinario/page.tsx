62a7d138a463a2ed1a1175d8c425d0eac9321999
Loki James <lordlooki18@gmail.com>
2025-08-14 20:51:00 +0000

    la pagina de control rutinario esta mucho mas lenta

--- src/app/(app)/ensayos/control-rutinario/page.tsx
+++ src/app/(app)/ensayos/control-rutinario/page.tsx
@@ -4,14 +4,7 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 import Loading from '../../loading';
-
-const ControlRutinarioPageClient = dynamic(
-  () => import('@/components/ensayos/control-rutinario-page'),
-  { 
-    loading: () => <Loading />,
-    ssr: false 
-  }
-);
+import ControlRutinarioPageClient from '@/components/ensayos/control-rutinario-page';
 
 export default function ControlRutinarioPage() {
   return <ControlRutinarioPageClient />;
