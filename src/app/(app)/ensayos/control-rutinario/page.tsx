c828d76d4bd3b98f3cff44cb449ca0a5a29354de
Loki James <lordlooki18@gmail.com>
2025-08-18 20:14:57 +0000

    la carga de las paginas desde las tarjetas del dashboard y desde selecci

--- src/app/(app)/ensayos/control-rutinario/page.tsx
+++ src/app/(app)/ensayos/control-rutinario/page.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 import Loading from '../../loading';
 
-const DynamicControlRutinario = dynamic(() => import('@/components/ensayos/control-rutinario-page'), {
+const DynamicControlRutinario = dynamic(() => import('@/components/ensayos/control-rutinario-page').then(mod => mod.default), {
   loading: () => <Loading />,
   ssr: false
 });
