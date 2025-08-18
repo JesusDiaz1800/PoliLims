5d2eb9695440c5a479351a16270466d9a518c3fa
Loki James <lordlooki18@gmail.com>
2025-08-18 20:09:14 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,4 +1,5 @@
 
+
 /**
  * @interface User
  * @description Defines the structure of a user object in the application.
@@ -22,84 +23,84 @@ const users: Record<string, User> = {
         fullName: "Jesus Diaz",
         role: "Ing. Analista de Calidad",
         initials: "JD",
-        avatarUrl: "https://placehold.co/40x40/E6E16B/313437?text=JD"
+        avatarUrl: "https://placehold.co/40x40.png"
     },
     "mmiranda": {
         username: "mmiranda",
         fullName: "Maximiliano Miranda",
         role: "Ing. Analista de Calidad",
         initials: "MM",
-        avatarUrl: "https://placehold.co/40x40/B39E83/313437?text=MM"
+        avatarUrl: "https://placehold.co/40x40.png"
     },
     "afigueroa": {
         username: "afigueroa",
         fullName: "Antonia Figueroa",
         role: "Analista de Calidad",
         initials: "AF",
-        avatarUrl: "https://placehold.co/40x40/D18E8E/313437?text=AF"
+        avatarUrl: "https://placehold.co/40x40.png"
     },
     "rcordova": {
         username: "rcordova",
         fullName: "Robinson Córdova",
         role: "Analista de Calidad",
         initials: "RC",
-        avatarUrl: "https://placehold.co/40x40/70A1AF/313437?text=RC"
+        avatarUrl: "https://placehold.co/40x40.png"
     },
     "bvasquez": {
         username: "bvasquez",
         fullName: "Bryan Vásquez",
         role: "Analista de Calidad",
         initials: "BV",
-        avatarUrl: "https://placehold.co/40x40/9D88B3/313437?text=BV"
+        avatarUrl: "https://placehold.co/40x40.png"
     },
     "jefe.calidad": {
         username: "jefe.calidad",
         fullName: "Victor Lutz",
         role: "Jefe de Calidad",
         initials: "VL",
-        avatarUrl: "https://placehold.co/40x40/E6B46B/313437?text=VL"
+        avatarUrl: "https://placehold.co/40x40.png"
     },
      "eibanez": {
         username: "eibanez",
         fullName: "Elias Ibañez",
         role: "Inspector de Calidad",
         initials: "EI",
-        avatarUrl: "https://placehold.co/40x40/6BE69C/313437?text=EI"
+        avatarUrl: "https://placehold.co/40x40.png"
     },
     "cmontellano": {
         username: "cmontellano",
         fullName: "Cristian Montellano",
         role: "Inspector de Calidad",
         initials: "CM",
-        avatarUrl: "https://placehold.co/40x40/6BA7E6/313437?text=CM"
+        avatarUrl: "https://placehold.co/40x40.png"
     },
     "dpalma": {
         username: "dpalma",
         fullName: "Daniel Palma",
         role: "Inspector de Calidad",
         initials: "DP",
-        avatarUrl: "https://placehold.co/40x40/E66BA7/313437?text=DP"
+        avatarUrl: "https://placehold.co/40x40.png"
     },
     "lparada": {
         username: "lparada",
         fullName: "Luis Parada",
         role: "Inspector de Calidad",
         initials: "LP",
-        avatarUrl: "https://placehold.co/40x40/E6826B/313437?text=LP"
+        avatarUrl: "https://placehold.co/40x40.png"
     },
     "admin": {
         username: "admin",
         fullName: "Administrador del Sistema",
         role: "Administrador",
         initials: "AD",
-        avatarUrl: "https://placehold.co/40x40/888/fff?text=AD"
+        avatarUrl: "https://placehold.co/40x40.png"
     },
     "cliente": {
         username: "cliente",
         fullName: "Cliente de Ejemplo",
         role: "Cliente",
         initials: "CE",
-        avatarUrl: "https://placehold.co/40x40/3498db/fff?text=CE"
+        avatarUrl: "https://placehold.co/40x40.png"
     }
 };
 
