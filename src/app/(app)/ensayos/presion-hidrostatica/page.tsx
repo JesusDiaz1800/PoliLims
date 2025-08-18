85af50880e006a3714da9a5f8ef25f6bba25225c
Loki James <lordlooki18@gmail.com>
2025-08-18 18:52:44 +0000

    la pagina del ensyo de presion hidrostatica se queda cargando pero no mu

--- src/app/(app)/ensayos/presion-hidrostatica/page.tsx
+++ src/app/(app)/ensayos/presion-hidrostatica/page.tsx
@@ -4,11 +4,7 @@
 import * as React from 'react';
 import dynamic from 'next/dynamic';
 import Loading from '@/app/(app)/loading';
-
-const ResistenciaPresionHidrostaticaPageContent = dynamic(
-  () => import('@/components/ensayos/phi/resistencia-presion-hidrostatica-content'),
-  { loading: () => <Loading />, ssr: false }
-);
+import ResistenciaPresionHidrostaticaPageContent from '@/components/ensayos/phi/resistencia-presion-hidrostatica-content';
 
 export default function ResistenciaPresionHidrostaticaPage() {
     return <ResistenciaPresionHidrostaticaPageContent />;
